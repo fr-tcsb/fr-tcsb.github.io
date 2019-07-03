607c1b507d31615ea5e50573f3452403b577fe31
froomboyo <52464085+fr-tcsb@users.noreply.github.com>
2019-07-03 14:29:17 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function greetMe(name) {
+  var today = new Date().toDateString();
+  console.log("Hello " + name + ", today is " + today);
+}
+
+greetMe("Person!");
+
+// Select an element with id="button"
+var element = document.querySelector('#button');
+
+element.addEventListener("click", function() {
+  document.getElementById("https://www.youtube.com/channel/UCmbKYxXpaHkE2GDsMK-K_2w")
+});
